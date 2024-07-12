568bd041930d30f416aacdd664cdc256060b0ad0
JulianVidal <julividal03@hotmail.com>
2024-07-12 07:49:57 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Juliv58
+
+A 58 key layout for the lily58 keyboard. Custom symbols layer, numpad, navigation layer and function layer.
+
+# How to compile
+
+
--- keymap.c
+++ keymap.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |  -   |  4   |  5   |  6   |  =   |-------.    ,-------|      |RCTRL |RSHIFT| RALT | RGUI |      |
  * |------+------+-------------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |  :   |  1   |  2   |  3   |  /   |-------|    |-------|      |      |      |      |      |      |
+ * |      |      |  1   |  2   |  3   |  /   |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/      /     \      \-----------------------------------------'
- *                  |      |  .   |   0   | /      /       \       \ |      |      |      |
+ *                  |      |      |   0   | /  .   /       \       \ |      |      |      |
  *                  |      |      |       |/      /         \      \ |      |      |      |
  *                  `----------------------------'           '------''--------------------'
  */
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    | PRV  |VOLDW |VOLUP | NXT  |      |      |
+ * |      |      |      |      |      |      |                    | PRV  |VOLDW |VOLUP | NXT  | ST/OP|      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      | LGUI | LAlt |LSHIFT| LCTRL|      |-------.    ,-------| Left | Down |  Up  |Right | TAB  |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_NAV] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, XXXXXXX,
   XXXXXXX, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TAB,  XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
                              XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
